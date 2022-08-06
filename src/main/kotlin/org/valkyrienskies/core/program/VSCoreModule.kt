1b707c4ed5f7389ab6c9657f8df9bad122013f35
Rubydesic <rubydesic@gmail.com>
2022-08-05 22:28:16 -0400

    create VSJacksonModule

--- src/main/kotlin/org/valkyrienskies/core/program/VSCoreModule.kt
+++ src/main/kotlin/org/valkyrienskies/core/program/VSCoreModule.kt
@@ -5,8 +5,9 @@ import dagger.Provides
 import org.valkyrienskies.core.hooks.AbstractCoreHooks
 import org.valkyrienskies.core.networking.VSNetworking.NetworkingModule
 import org.valkyrienskies.core.networking.VSNetworkingConfigurator
+import org.valkyrienskies.core.util.serialization.VSJacksonModule
 
-@Module(includes = [NetworkingModule::class])
+@Module(includes = [NetworkingModule::class, VSJacksonModule::class])
 class VSCoreModule(
     @get:Provides val hooks: AbstractCoreHooks,
     @get:Provides val configurator: VSNetworkingConfigurator
