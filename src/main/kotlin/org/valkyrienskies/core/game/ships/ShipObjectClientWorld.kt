70fdfdd0e4a42949e1c12e8fb2ce0cbb6ea71725
Rubydesic <rubydesic@gmail.com>
2022-08-06 12:47:24 -0400

    added WorldScoped

--- src/main/kotlin/org/valkyrienskies/core/game/ships/ShipObjectClientWorld.kt
+++ src/main/kotlin/org/valkyrienskies/core/game/ships/ShipObjectClientWorld.kt
@@ -16,6 +16,7 @@ interface ShipObjectClientWorldComponent {
     }
 }
 
+@WorldScoped
 class ShipObjectClientWorld @Inject constructor(
     networkManagerFactory: ShipObjectNetworkManagerClient.Factory
 ) : ShipObjectWorld<ShipObjectClient>() {
