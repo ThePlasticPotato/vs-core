26fb438af574eb38839e1284537ff0ba1850fb92
Rubydesic <rubydesic@gmail.com>
2022-10-25 18:16:37 -0400

    And another test case for memory usage to DenseBlockPosSetTest

--- src/test/kotlin/org/valkyrienskies/core/datastructures/DenseBlockPosSetTest.kt
+++ src/test/kotlin/org/valkyrienskies/core/datastructures/DenseBlockPosSetTest.kt
@@ -3,14 +3,24 @@ package org.valkyrienskies.core.datastructures
 import io.kotest.assertions.withClue
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.collections.shouldBeIn
+import io.kotest.matchers.ints.shouldBeInRange
 import io.kotest.matchers.shouldBe
 import io.kotest.property.Arb
 import io.kotest.property.arbitrary.set
 import io.kotest.property.checkAll
 import org.joml.Vector3i
 import org.valkyrienskies.test_utils.generators.vector3i
+import kotlin.random.Random
 
 class DenseBlockPosSetTest : StringSpec({
+
+    "should be able to store a lot of blocks" {
+        val set = DenseBlockPosSet()
+        for (i in 0..10_000_000) {
+            set.add(Random.nextInt(-10_000, 10_000), Random.nextInt(-10_000, 10_000), Random.nextInt(-10_000, 10_000))
+        }
+        set.size shouldBeInRange 9_000_000..10_000_000
+    }
 
     "should iterate blocks" {
         val blocksArb = Arb.set(Arb.vector3i())
