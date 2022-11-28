3667c4e25cc1ccb142022226ed4b35cc2fbb999d
StewStrong <stew.strong.420@gmail.com>
2022-11-28 07:34:05 -0700

    Fixed Krunch not working on aarch64

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,7 +47,7 @@ dependencies {
     val kotestVersion = "5.4.1"
 
     // VS Physics
-    api("org.valkyrienskies:physics_api_krunch:1.0.0+42dbe57aac")
+    api("org.valkyrienskies:physics_api_krunch:1.0.0+bcbbe44ea9")
 
     // JOML for Math
     api("org.joml:joml:1.10.4")
