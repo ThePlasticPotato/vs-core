7502f9ff2ac406c59148dc9dfc294489225ff16c
StewStrong <stew.strong.420@gmail.com>
2023-03-21 23:29:07 -0600

    Fixed slide constraint

--- impl/build.gradle.kts
+++ impl/build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
     val kotestVersion = "5.4.1"
 
     // VS Physics
-    api("org.valkyrienskies:physics_api_krunch:1.0.0+eb3ae45554")
+    api("org.valkyrienskies:physics_api_krunch:1.0.0+191b400c46")
 
     // JOML for Math
     api("org.joml:joml:1.10.4")
