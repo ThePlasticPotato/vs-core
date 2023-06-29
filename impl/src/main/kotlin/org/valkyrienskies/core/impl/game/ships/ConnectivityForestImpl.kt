dda091ce99a672477c9636ad08957e8bfbde6633
ThePlasticPotato <77798144+ThePlasticPotato@users.noreply.github.com>
2023-06-29 18:06:24 -0400

    ooooo

--- impl/src/main/kotlin/org/valkyrienskies/core/impl/game/ships/ConnectivityForestImpl.kt
+++ impl/src/main/kotlin/org/valkyrienskies/core/impl/game/ships/ConnectivityForestImpl.kt
@@ -1,14 +1,18 @@
 package org.valkyrienskies.core.impl.game.ships
 
+import it.unimi.dsi.fastutil.objects.ObjectArrayList
 import org.joml.Vector3dc
 import org.joml.Vector3ic
 import org.valkyrienskies.core.impl.datastructures.BlockPos2ObjectOpenHashMap
+import org.valkyrienskies.core.impl.datastructures.DenseBlockPosSet
 import org.valkyrienskies.core.impl.datastructures.dynconn.BlockPosVertex
 import org.valkyrienskies.core.impl.datastructures.dynconn.ConnGraph
 import org.valkyrienskies.core.impl.datastructures.dynconn.ConnVertex
+import java.util.Queue
 
 class ConnectivityForestImpl(override val graph: ConnGraph,
-    override val vertices: BlockPos2ObjectOpenHashMap<BlockPosVertex>
+    override val vertices: BlockPos2ObjectOpenHashMap<BlockPosVertex>,
+    override val breakages: MutableSet<Pair<Vector3ic, Vector3ic>>
 ) : ConnectivityForest {
 
     override fun newVertex(posX: Int, posY: Int, posZ: Int): Boolean {
@@ -57,8 +61,29 @@ class ConnectivityForestImpl(override val graph: ConnGraph,
         return true
     }
 
-    override fun split(vectorOne: Vector3ic, vectorTwo: Vector3ic): Boolean {
-        return false
+    override fun split(vectorOne: Vector3ic, vectorTwo: Vector3ic): Pair<BlockPos2ObjectOpenHashMap<BlockPosVertex>, Boolean> {
+        val vertexOne = vertices.get(vectorOne.x(), vectorOne.y(), vectorOne.z())
+        val vertexTwo = vertices.get(vectorTwo.x(), vectorTwo.y(), vectorTwo.z())
+
+        val connectedToOne = BlockPos2ObjectOpenHashMap<BlockPosVertex>()
+        val connectedToTwo = BlockPos2ObjectOpenHashMap<BlockPosVertex>()
+
+        if (vertexOne != null && vertexTwo != null) {
+            vertices.forEach{x, y, z, vertex -> Unit
+                if (graph.connected(vertexOne, vertex)) {
+                    connectedToOne.put(x, y, z, vertex)
+                }
+                if (graph.connected(vertexTwo, vertex)) {
+                    connectedToTwo.put(x, y, z, vertex)
+                }
+            }
+        }
+
+        if (connectedToOne.values.size > connectedToTwo.values.size) {
+            return Pair(connectedToTwo, true)
+        } else {
+            return Pair(connectedToOne, false)
+        }
     }
 
     override fun merge() {
