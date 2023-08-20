e28f9f47a0344d04a44e4e3fbda872d381df3396
ThePlasticPotato <77798144+ThePlasticPotato@users.noreply.github.com>
2023-08-19 23:44:30 -0400

    removed air pockets... for now.

--- impl/src/main/kotlin/org/valkyrienskies/core/impl/game/ships/ShipObjectServer.kt
+++ impl/src/main/kotlin/org/valkyrienskies/core/impl/game/ships/ShipObjectServer.kt
@@ -2,7 +2,6 @@ package org.valkyrienskies.core.impl.game.ships
 
 import com.google.common.collect.MutableClassToInstanceMap
 import org.joml.Vector3ic
-import org.joml.primitives.AABBi
 import org.valkyrienskies.core.api.ships.WingManager
 import org.valkyrienskies.core.apigame.ShipTeleportData
 import org.valkyrienskies.core.impl.api.LoadedServerShipInternal
@@ -46,10 +45,10 @@ class ShipObjectServer(
         val wingManager = WingManagerImpl()
         wingManager.createWingGroup()
         val connManager = ConnectivityForestImpl(ConnGraph(), HashMap<Vector3ic, BlockPosVertex>(), mutableSetOf(), mutableSetOf(), mutableSetOf())
-        val airManager = AirPocketForestImpl(ConnGraph(), HashMap<Vector3ic, BlockPosVertex>(), HashMap<Vector3ic, BlockPosVertex>(), HashMap<Vector3ic, BlockPosVertex>(), mutableSetOf(), AABBi(shipAABB))
+        // val airManager = AirPocketForestImpl(ConnGraph(), HashMap<Vector3ic, BlockPosVertex>(), HashMap<Vector3ic, BlockPosVertex>(), HashMap<Vector3ic, BlockPosVertex>(), mutableSetOf(), AABBi(shipAABB))
         setAttachment(WingManager::class.java, wingManager)
         setAttachment(ConnectivityForest::class.java, connManager)
-        setAttachment(AirPocketForest::class.java, airManager)
+        // setAttachment(AirPocketForest::class.java, airManager)
     }
 
     override fun <T> setAttachment(clazz: Class<T>, value: T?) {
